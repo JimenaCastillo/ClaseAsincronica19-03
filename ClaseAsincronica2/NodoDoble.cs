01f3fa333dd9785d4aff953da62c54ad56f8f5ac
unknown <j.castillo.2@estudiantec.cr>
2025-03-20 21:10:22 -0600

    Codigo completo

---
+++ ClaseAsincronica2/NodoDoble.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAsincronica2
+{
+    class NodoDoble
+    {
+        private int Dato;
+        private NodoDoble Siguiente;
+        private NodoDoble Anterior;
+
+        public int dato
+        {
+            get { return Dato; }
+            set { Dato = value; }
+        }
+
+        public NodoDoble siguiente
+        {
+            get { return Siguiente; }
+            set { Siguiente = value; }
+        }
+        //
+        public NodoDoble anterior
+        {
+            get { return Anterior; }
+            set { Anterior = value; }
+        }
+    }
+}
+
+//commit
---
+++ ClaseAsincronica2/NodoSimple.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseAsincronica2
+{
+    class NodoSimple
+    {
+        private int Dato;
+        private NodoSimple Siguiente;
+
+        public int dato
+        {
+            get { return Dato; }
+            set { Dato = value; }
+        }
+
+        public NodoSimple siguiente
+        {
+            get { return Siguiente; }
+            set { Siguiente = value; }
+        }
+    }
+}
+//
+//commit
